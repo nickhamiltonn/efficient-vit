e2db9dd7c855f8c4df95d191b65949860cdeacdd
Nick Hamilton <nick.hamilton@shaw.ca>
2023-04-19 13:55:11 -0700

    Another attempt with the attention added in

--- models_v3.py
+++ models_v3.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from functools import partial
 
-from timm.models.vision_transformer import Attention, Mlp, _cfg
+from timm.models.vision_transformer import Mlp, _cfg
 from timm.models.registry import register_model
 from timm.models.layers import DropPath, trunc_normal_, to_2tuple
 
@@ -12,6 +12,34 @@ from timm.models.layers import DropPath, trunc_normal_, to_2tuple
 __all__ = [
     'baselineViT_small_patch16_224', 'localViT_small_patch16_224'
 ]
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
 
 
 class PatchEmbed(nn.Module):
